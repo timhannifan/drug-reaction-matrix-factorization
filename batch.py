d274563947ed58eb7ca3f96ff52e51570c733b1b
Tim Hannifan <hannifan@gmail.com>
2019-12-13 20:37:27 -0600

    cleanup batch.py and code and rm old data

--- batch.py
+++ batch.py
@@ -1,9 +1,8 @@
 """
 This file writes a limited batch of the full dataset to a new csv. In order to run, you must have the decompressed version on your machine.
 
-Run example:
+Example:
     python3 batch.py
-
 """
 
 import os
@@ -13,7 +12,7 @@ DATA_PATH = '../'
 INPUT_FNAME = 'TWOSIDES.csv'
 OUTPUT_FNAME = 'twosides-lg.csv'
 
-# every 10th line = 10% of the lines
+# Every 10th line = 10% of the lines
 n = 10
 df = pd.read_csv(
     os.path.join(DATA_PATH, INPUT_FNAME),
@@ -22,10 +21,6 @@ df = pd.read_csv(
     'condition_concept_name', 'A', 'B', 'C', 'D',
     'PRR', 'PRR_error', 'mean_reporting_frequency'],
     low_memory=False)
-
-# # Fill null values with zeros
-# df['PRR'] = pd.to_numeric(df['PRR'], errors='coerce')
-# df['PRR_error'] = pd.to_numeric(df['PRR'], errors='coerce')
 
 # Convert numeric cols, fillna with zero
 to_numeric = ['A', 'B', 'C', 'D', 'PRR', 'PRR_error',
@@ -40,13 +35,9 @@ tolower = ['condition_concept_name', 'drug_1_concept_name',
 for col in tolower:
     df[col] = df[col].str.lower()
 
-
 # Concatenate the drug names to form a single entitiy, convert to lowercase
 df['joint_drug_name'] = (df['drug_1_concept_name'] + "_" +
     df['drug_2_concept_name'])
 
-
 # Write to new file
 df.to_csv(OUTPUT_FNAME, index=False)
-
-
