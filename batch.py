7a4ea5acfeef475a8f74266d2eb2d04034dd7b31
Tim Hannifan <hannifan@gmail.com>
2019-12-13 20:34:47 -0600

    rewriting batch to get propensity scores instead of PRR. adding new sample data files

--- batch.py
+++ batch.py
@@ -3,33 +3,50 @@ This file writes a limited batch of the full dataset to a new csv. In order to r
 
 Run example:
     python3 batch.py
+
 """
 
 import os
-
 import pandas as pd
 
 DATA_PATH = '../'
-FILE_NAME = 'TWOSIDES.csv'
-n = 10  # every 10th line = 10% of the lines
+INPUT_FNAME = 'TWOSIDES.csv'
+OUTPUT_FNAME = 'twosides-lg.csv'
+
+# every 10th line = 10% of the lines
+n = 10
 df = pd.read_csv(
-    os.path.join(DATA_PATH, FILE_NAME),
+    os.path.join(DATA_PATH, INPUT_FNAME),
     header=0, skiprows=lambda i: i % n != 0,
     usecols=['drug_1_concept_name', 'drug_2_concept_name',
-    'condition_concept_name', 'PRR', 'PRR_error'],
+    'condition_concept_name', 'A', 'B', 'C', 'D',
+    'PRR', 'PRR_error', 'mean_reporting_frequency'],
     low_memory=False)
 
-# Fill null values with zeros
-df['PRR'] = pd.to_numeric(df['PRR'], errors='coerce')
-df['PRR_error'] = pd.to_numeric(df['PRR'], errors='coerce')
+# # Fill null values with zeros
+# df['PRR'] = pd.to_numeric(df['PRR'], errors='coerce')
+# df['PRR_error'] = pd.to_numeric(df['PRR'], errors='coerce')
+
+# Convert numeric cols, fillna with zero
+to_numeric = ['A', 'B', 'C', 'D', 'PRR', 'PRR_error',
+    'mean_reporting_frequency']
+for col in to_numeric:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
 df = df.fillna(0)
 
+# Convert all string rows to lowercase
+tolower = ['condition_concept_name', 'drug_1_concept_name',
+    'drug_2_concept_name']
+for col in tolower:
+    df[col] = df[col].str.lower()
+
+
 # Concatenate the drug names to form a single entitiy, convert to lowercase
-df['joint_drug_name'] = df['drug_1_concept_name'] + "_" + df['drug_2_concept_name']
-df['joint_drug_name'] = df['joint_drug_name'].str.lower()
-df['condition_concept_name'] = df['condition_concept_name'].str.lower()
+df['joint_drug_name'] = (df['drug_1_concept_name'] + "_" +
+    df['drug_2_concept_name'])
+
 
 # Write to new file
-df[['joint_drug_name', 'condition_concept_name', 'PRR', 'PRR_error']].to_csv('twosides-10pct.csv')
+df.to_csv(OUTPUT_FNAME, index=False)
 
 
